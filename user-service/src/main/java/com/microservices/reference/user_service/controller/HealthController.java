c6ace21d3d0ba917479c70fa2838f67116822ff1
nielson.ferreira <nielson.ferreira@mv.com.br>
2025-08-17 10:19:49 -0300

    Ajustado balanceamento de requisições via gateway

--- user-service/src/main/java/com/microservices/reference/user_service/controller/HealthController.java
+++ user-service/src/main/java/com/microservices/reference/user_service/controller/HealthController.java
@@ -3,9 +3,11 @@ package com.microservices.reference.user_service.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/health")
 @Tag(name = "Health", description = "Endpoints de verificação de saúde")
 public class HealthController {
 
